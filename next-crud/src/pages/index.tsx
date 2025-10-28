319c7f059550ea0acd1125df5195c665a7d8b38a
Lucas Nascimento <lucabelezal@gmail.com>
2025-10-28 10:45:51 -0300

    fix: corrigir import de globals.css e adicionar alias @ no tsconfig

---
+++ next-crud/src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+    return (
+        <div className={
+            'flex items-center justify-center h-screen'
+        }>
+            <h1 className={'text-4xl font-bold'}>Welcome to the Home Page</h1>
+        </div>
+    );
+}
