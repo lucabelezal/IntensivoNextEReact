dc6e1daa72810bc2376aee7d2d35eb6258b41571
Lucas Nascimento <lucabelezal@gmail.com>
2025-10-30 13:22:37 -0300

    chore: commit automático

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,11 +1,21 @@
+import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 
 export default function Home() {
+
+    const clientes = [
+        new Cliente('Ana', 3456, '1'),
+        new Cliente('Maria', 1234, '2'),
+        new Cliente('João', 5678, '3'),
+    ]
+
     return (
-        <div className={
-            'flex items-center justify-center h-screen'
-        }>
-            <h1 className={'text-4xl font-bold'}>Welcome to the Home Page</h1>
+        <div className={'flex items-center justify-center h-screen'}>
+            <Layout titulo="Welcome to the Home Page">
+                <Tabela clientes={clientes} />
+            </Layout>
         </div>
     );
 }
